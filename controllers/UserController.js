167b61f1e99985e55340c3514edb0aee08b823f7
wladislas.gorka <wladislas.gorka@gmail.com>
2024-12-05 09:27:16 +0100

    Insert User

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const userView = require("../views/UserView");
 const loginView = require("../views/LoginView");
 const registerView = require("../views/RegisterView");
+const db = require("../db/db");
 
 function getUser(req, res){
     const user = new User(1,"Wladislas");
@@ -30,7 +31,20 @@ function showRegister(req, res){
 function traiteRegister(req, res){
     const {nom,password} = req.body;
     const newUser = new User(nom,password);
-    res.cookie("users",newUser);
+    //console.log(newUser);
+    const query = 'INSERT INTO users (username,password) VALUES (?, ?)';
+    db.run(query,[newUser.name,newUser.password], 
+        function (err){
+            if(err){
+                console.error('register échoué: ',err.message);
+                res.send("ERROR");
+            }else{
+                console.log("user access: ",newUser);
+                res.send("SUCCESS");
+            }
+        }
+    )    
 }
+
 
 module.exports={getUser,showLogin,traiteLogin,showRegister,traiteRegister};
--- db/db.js
+++ db/db.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = sqlite3.Database('./database.sqlite',(err)=>{
+const db = new sqlite3.Database('./database.sqlite',(err)=>{
     if(err){
         console.error('erreur de connexion :',err.message);
     }else{
