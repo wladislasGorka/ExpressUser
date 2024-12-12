47e99f50673773b034ce05b241e3603ae68b351e
wladislas.gorka <wladislas.gorka@gmail.com>
2024-12-12 12:33:05 +0100

    button panier dans index

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,9 +1,23 @@
-const Annonce = require("../models/Annonce");
 const db = require("../db/db");
 
+function checkdansPanier(userId,annonceId){
+    return new Promise((resolve, reject) => {
+        const query = 'SELECT * FROM paniers WHERE userId=? AND annonceId=?';
+        db.get(query, [userId, annonceId], (err, row) => {
+            if (err) {
+                console.error('Erreur showAnnonce');
+                return reject(err);
+            }
+            if (row) {
+                return resolve(true);
+            }
+            return resolve(false);
+        });
+    });
+}
 function showAnnonces(req, res){
-    const query = "SELECT * FROM annonces";
-    db.all(query,[],(err,rows) => {
+    const query = "SELECT * FROM annonces LEFT JOIN paniers ON annonces.id=paniers.annonceId AND userId=?";
+    db.all(query,[req.session.userId],(err,rows) => {
         if(err){
             console.error('Erreur: showAnnoncesView request');
             throw err;
